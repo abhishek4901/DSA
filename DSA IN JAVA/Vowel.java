3b4aca90675c0be3cfc9f97949360890563cd81f
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-10-02 01:57:48 +0530

    .

---
+++ DSA IN JAVA/Vowel.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Vowel{
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the string to  count the vowel ");
+        int count =0;
+        String str = sc.nextLine();
+        for(int i =0; i<str.length(); i++){
+            char ch = str.charAt(i);//for single word (string ko ek ek word me todne liye )
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                count++;
+            }
+        }
+        System.out.println(count);
+    
+
+       
+    }
+}
