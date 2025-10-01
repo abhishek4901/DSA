01c2174da050f16c6076e216d16b4ecd31b77eb1
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-10-02 03:33:49 +0530

    .

--- DSA IN JAVA/inbuildMethod.java
+++ DSA IN JAVA/inbuildMethod.java
@@ -2,12 +2,17 @@ public class inbuildMethod {
     public static void main(String[] args) {
         String s = "raja";
         String r = "ram";
+        String a = "hello";
         System.out.println(s.concat(r));//rajaram
         //or
         s= s.concat(r);
         System.out.println(s);//rajaram
         //+oprator
+        s=s+a;
+        System.out.println(s);//rajaramhello s me pahle se rajaram store h 
+        //or
         s= s+"raghupati";
         System.out.println(s);//rajaramraghupati
+
     }
 }
