44eaabd99c3abc431b19229f11535c5d17758e6d
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-10-09 14:11:53 +0530

    Update AlignGivenZeroOneSide.c

--- DSA IN C/Array/AlignGivenZeroOneSide.c
+++ DSA IN C/Array/AlignGivenZeroOneSide.c
@@ -9,7 +9,7 @@ int main (){
      
         scanf("%d",&arr[i]);
         }
-    
+     
     for(i=0;i<size;i++){
         for(j=i+1;j<size;j++){//for(j=0;j<size;j++){ but that increases time complexity
             if(arr[i]<arr[j]){ 
