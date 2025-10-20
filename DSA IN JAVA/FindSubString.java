d3295fac57b183d74c1f27d02389e701224bf7cb
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-10-21 04:36:38 +0530

    Fix loop structure in FindSubString.java

--- DSA IN JAVA/FindSubString.java
+++ DSA IN JAVA/FindSubString.java
@@ -2,7 +2,7 @@
     public static void main(String[] args) { 
         int count =0;
         String s = "raj"; 
-        for (int i = 0; i <s.length(); i++) {  
+        for (int i = 0; i <s.length(); i++) {   
         for (int j = i+1; j <=s.length(); j++) { 
             System.out.println(s.substring(i,j)); 
             count++;  
