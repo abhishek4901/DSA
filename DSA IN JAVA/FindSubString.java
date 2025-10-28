7c420b318a3ab127d8b2a5e1848ea35dd84cac8e
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-10-28 20:29:29 +0530

    Update FindSubString.java

--- DSA IN JAVA/FindSubString.java
+++ DSA IN JAVA/FindSubString.java
@@ -1,6 +1,6 @@
  public class FindSubString {
     public static void main(String[] args) { 
-        int count =0;
+        int count =0; 
         String s = "raj"; 
         for (int i = 0; i <s.length(); i++) {   
         for (int j = i+1; j <=s.length(); j++) { 
